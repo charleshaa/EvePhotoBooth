eba9c9419b0277b9d4520b4160849d6071bed9bf
Carlos <charleshaarman@gmail.com>
2017-08-16 21:37:42 +0200

    Multiple uploads, preview, caption upload

--- public/js/my-app.js
+++ public/js/my-app.js
@@ -1,7 +1,7 @@
 const isMobile = () => {
-  var check = false;
-  (function(a){if(/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino/i.test(a)||/1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0,4))) check = true;})(navigator.userAgent||navigator.vendor||window.opera);
-  return check;
+    var check = false;
+    ( function ( a ) { if ( /(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino/i.test( a ) || /1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test( a.substr( 0, 4 ) ) ) check = true; } )( navigator.userAgent || navigator.vendor || window.opera );
+    return check;
 };
 
 
@@ -162,24 +162,96 @@ const createPreview = img => {
 
 const canUpload = () => window.File && window.FileReader && window.FormData;
 
-const uploadAll = () => {
+const startUpload = () => {
+
+};
+
+const progress = ( e, index ) => {
+
+    if ( e.lengthComputable ) {
+        var max = e.total;
+        var current = e.loaded;
+
+        var Percentage = ( current * 100 ) / max;
+        console.log( Percentage );
+        if ( Percentage >= 100 ) {
+            console.log("Process is at 100 !");
+        }
+    }
+}
+
+const upload = ( index ) => {
+
+    var picture = queue[ index ];
+
+    var formData = new FormData();
+
+    formData.append( 'image', picture.file );
+    formData.append( 'caption', picture.caption );
+
+    myApp.showPreloader("Uploading image...");
+
+
+    $.ajax( {
+        type: 'POST',
+        url: '/upload',
+        data: formData,
+        xhr: function () {
+            var myXhr = $.ajaxSettings.xhr();
+            if ( myXhr.upload ) {
+                myXhr.upload.addEventListener( 'progress', (e) => {
+                    progress(e, index);
+                }, false );
+            }
+            return myXhr;
+        },
+        cache: false,
+        contentType: false,
+        processData: false,
+
+        success: function ( data ) {
+            console.log( data );
+            myApp.hidePreloader();
+            console.log( 'data returned successfully' );
+            myApp.modal( {
+                title: 'Upload successful',
+                afterText: `
+                    <div class="picture-preview">
+                        <img style="max-width: 100%;" src="${data.url}" />
+                    </div>
+                `,
+                buttons: [ {
+                        text: 'OK'
+                    }
+                ]
+            } );
+        },
+
+        error: function ( data ) {
+            console.log( data );
+        }
+    } );
+
+
 
 };
 
 const toggleDone = () => {
-    if(queue.length > 0){
-        $('#upload-all').show();
+    if ( queue.length > 0 ) {
+        $( '#upload-all' ).show();
     } else {
-        $('#upload-all').hide();
+        $( '#upload-all' ).hide();
     }
 };
+
+
 
 $( document ).ready( function () {
     // init
 
-    if(!isMobile()){
-        console.log("Desktop");
-        $('#image-upload-input').attr('multiple', 'multiple');
+    if ( !isMobile() ) {
+        console.log( "Desktop" );
+        $( '#image-upload-input' ).attr( 'multiple', 'multiple' );
     }
 
     setTimeout( () => {
@@ -211,8 +283,8 @@ $( document ).ready( function () {
     var adds = 0;
     document.getElementById( "image-upload-input" ).onchange = function ( e ) {
         var files = e.target.files;
-        for(let i=0, l=queue.length, f; f = files[i]; i++){
-            console.log(f.name);
+        for ( let i = 0, l = queue.length, f; f = files[ i ]; i++ ) {
+            console.log( f.name );
             EXIF.getData( f, function () {
 
                 // switch ( imgObject.orientation ) {
@@ -250,16 +322,16 @@ $( document ).ready( function () {
                 //         break;
                 // }
                 queue.push( {
-                    time:           EXIF.getTag( this, "DateTime" ),
-                    height:         EXIF.getTag( this, "PixelYDimension" ),
-                    width:          EXIF.getTag( this, "PixelXDimension" ),
-                    orientation:    EXIF.getTag( this, "Orientation" ),
+                    time: EXIF.getTag( this, "DateTime" ),
+                    height: EXIF.getTag( this, "PixelYDimension" ),
+                    width: EXIF.getTag( this, "PixelXDimension" ),
+                    orientation: EXIF.getTag( this, "Orientation" ),
                     file: this
                 } );
                 var reader = new FileReader();
                 reader.onerror = () => alert( "Error reading file" );
-                reader.onloadend = (function(theFile){
-                    return function(e) {
+                reader.onloadend = ( function ( theFile ) {
+                    return function ( e ) {
                         var image = new Image();
                         var canvas = document.createElement( 'canvas' );
                         var context = canvas.getContext( '2d' );
@@ -285,9 +357,9 @@ $( document ).ready( function () {
                             var list = $( '#image-list' );
                             var item = $( '<li class="picture-details" />' );
                             var c = $( canvas );
-                            console.log("Indexes: queue length, i", queue.length, i);
-                            item.data( 'index',  l+i);
-                            queue[ l+i ].imageData = reader.result;
+                            console.log( "Indexes: queue length, i", queue.length, i );
+                            item.data( 'index', l + i );
+                            queue[ l + i ].imageData = reader.result;
                             var b64 = canvas.toDataURL( theFile.type );
                             item.css( 'backgroundImage', 'url(' + b64 + ')' );
                             list.append( item );
@@ -297,7 +369,7 @@ $( document ).ready( function () {
                         }
                         image.src = reader.result;
                     };
-                })(this);
+                } )( this );
                 reader.readAsDataURL( this );
             } );
         }
@@ -320,18 +392,17 @@ $( document ).ready( function () {
                     <textarea style="padding: 15px;" class="col-100" rows="5" name="" id="caption" placeholder="Add a caption">${prevCaption}</textarea>
                 </div>
             `,
-            buttons: [
-                {
+            buttons: [ {
                     text: 'Cancel'
                 },
                 {
                     text: 'Remove',
-                    onClick: function(){
-                        queue.splice(index, 1);
+                    onClick: function () {
+                        queue.splice( index, 1 );
                         item.remove();
-                        var items = $('.picture-details');
-                        for(let i=index; i<queue.length; i++){
-                            $(items[i]).data('index', parseInt($(items[i]).data('index')) - 1);
+                        var items = $( '.picture-details' );
+                        for ( let i = index; i < queue.length; i++ ) {
+                            $( items[ i ] ).data( 'index', parseInt( $( items[ i ] ).data( 'index' ) ) - 1 );
                         }
 
                     }
@@ -340,7 +411,7 @@ $( document ).ready( function () {
                     text: 'Save',
                     bold: true,
                     onClick: function () {
-                        queue[ index ].caption = $('#caption').val();
+                        queue[ index ].caption = $( '#caption' ).val();
                     }
                 },
             ]
@@ -348,5 +419,9 @@ $( document ).ready( function () {
 
     } );
     //input.on( 'change', onSelect );
-    $('body').on('modal:closed', toggleDone);
+    $( 'body' ).on( 'modal:closed', toggleDone );
+
+    $('body').on('click', '#upload-all', function(e){
+        upload(0);
+    });
 } );
